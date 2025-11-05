1ac0199ecfab6726dbbb0377b318abfd0d6238c7
Rahul <rahul.kumar@vegavid.com>
2025-11-05 14:36:37 +0530

    Remove dist directory from version control

--- dist/modals/Chat.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Chat.d.ts.map
--- dist/router/chatRouter.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const chatRouter: import("express-serve-static-core").Router;
-//# sourceMappingURL=chatRouter.d.ts.map
